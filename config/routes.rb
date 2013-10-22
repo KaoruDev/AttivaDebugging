0f5841844902418651e7d4557a03d4b044219bb3
Kaoru <dev@kaoruk.com>
2013-10-22 10:00:07 -0500

    Fix post to have a default arguement of 300

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Ativa::Application.routes.draw do
   devise_for :users
   resources :posts
   
-  resources :projects
+  resources :projects do
+    resources :subscriptions, only: [:create, :destroy]
+  end
 
   get 'users/:id' => 'users#show', as: 'user'
   root 'projects#index'
